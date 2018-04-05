87ab88c503fe91d1bb05b7698b8285f7a1122a87
lucasfrosty <lucas_frosty@hotmail.com>
2018-04-05 19:13:11 -0300

    1012

--- basics/1012.js
+++ basics/1012.js
@@ -1,5 +1,5 @@
-// var input = require('fs').readFileSync('/dev/stdin', 'utf8');
-// var lines = input.split('\n');
+var input = require('fs').readFileSync('/dev/stdin', 'utf8');
+var lines = input.split('\n');
 
 /**
  * Escreva a sua solução aqui
